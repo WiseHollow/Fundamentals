ddb3ad4dda6eaea307ad9106f4fc1d4e608440c8
WiseHollow <ieatlotz@gmail.com>
2018-08-08 16:13:38 -0600

    Economy update (same as the 1.12 commit).

--- src/com/WiseHollow/Fundamentals/Language.java
+++ src/com/WiseHollow/Fundamentals/Language.java
@@ -13,4 +13,8 @@ public class Language
     public static final String DoesNotHavePermission = PREFIX_WARNING + "You do not have permission for this function.";
     public static final String YouMustBeLoggedIn = PREFIX_WARNING + "You must be logged in to do this.";
     public static final String PlayerMustBeLoggedIn = PREFIX_WARNING + "That player must be logged in for this.";
+    public static final String MustBeANumber = PREFIX_WARNING + "What value you entered is not numerical.";
+    public static final String CannotSendYourselfMoney = PREFIX_WARNING + "You cannot send yourself money.";
+    public static final String SentMoney = PREFIX + "You sent %1 to %2.";
+    public static final String ReceivedMoney = PREFIX + "You received %1 from %2.";
 }
