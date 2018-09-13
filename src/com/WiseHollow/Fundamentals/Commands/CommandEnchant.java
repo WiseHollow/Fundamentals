65ce32828a471407d5b1dd919028140e64462f09
WiseHollow <ieatlotz@gmail.com>
2018-09-13 10:31:39 -0600

    Fixed unsafe enchantments. Added commentation to config.yml.

--- src/com/WiseHollow/Fundamentals/Commands/CommandEnchant.java
+++ src/com/WiseHollow/Fundamentals/Commands/CommandEnchant.java
@@ -16,26 +16,21 @@ import java.util.Arrays;
 /**
  * Created by John on 10/13/2016.
  */
-public class CommandEnchant implements CommandExecutor
-{
+public class CommandEnchant implements CommandExecutor {
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String cmd, String[] args)
-    {
-        if (!(sender instanceof Player))
-        {
+    public boolean onCommand(CommandSender sender, Command command, String cmd, String[] args) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage(Language.YouMustBeLoggedIn);
             return true;
         }
 
         Player player = (Player) sender;
-        if (!sender.hasPermission("Fundamentals.Enchant"))
-        {
+        if (!sender.hasPermission("Fundamentals.Enchant")) {
             player.sendMessage(Language.DoesNotHavePermission);
             return true;
         }
 
-        if (player.getInventory().getItemInMainHand() == null)
-        {
+        if (player.getInventory().getItemInMainHand() == null) {
             player.sendMessage(Language.PREFIX_WARNING + "You must have an item in your hand. ");
             return true;
         }
@@ -59,19 +54,23 @@ public class CommandEnchant implements CommandExecutor
             }
         }
 
-        if (enchantment == null)
-        {
+        if (enchantment == null) {
             player.sendMessage(Language.PREFIX_WARNING + "Invalid enchantment type!");
             return true;
         }
 
-        if (!Settings.AllowUnsafeEnchantments)
-            player.getInventory().getItemInMainHand().addEnchantment(enchantment, level);
-        else
+        if (!Settings.AllowUnsafeEnchantments) {
+            if (level > enchantment.getMaxLevel()) {
+                player.sendMessage(Language.PREFIX_WARNING + "The max level for that enchantment is: " + enchantment.getMaxLevel());
+                return true;
+            } else {
+                player.getInventory().getItemInMainHand().addEnchantment(enchantment, level);
+            }
+        } else {
             player.getInventory().getItemInMainHand().addUnsafeEnchantment(enchantment, level);
+        }
 
         player.sendMessage(Language.PREFIX + "Enchantment: " + enchantment.getKey().getKey() + " added at level " + level);
-
         return true;
     }
 }
